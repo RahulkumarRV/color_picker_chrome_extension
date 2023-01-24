6332a437afe0e710549426ba7db05dc7346bc4fc
= <rahulkumar@metafic.co>
2023-01-24 13:05:03 +0530

    version 1.0.0 completed

--- popup.js
+++ popup.js
@@ -3,20 +3,14 @@ const saveColorBtn = document.getElementById("save-color-btn");
 
 saveColorBtn.addEventListener("click", () => {
   chrome.storage.sync.set({ selectedColor: colorPicker.value }, () => {
-    if(chrome.runtime.lastError){
-	console.error("error is : ", chrome.runtime.lastError.message);
-    }
-    console.log("-> ");
+    
   });
 });
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    console.log(
-      `Storage key "${key}" in namespace "${namespace}" changed.`,
-      `Old value was "${oldValue}", new value is "${newValue}".`
-    );
 	document.body.style.backgroundColor = newValue;
+	document.querySelector("#color-code").innerHTML = "color code " + newValue;
   }
 });
 
@@ -24,5 +18,6 @@ window.addEventListener("load", () => {
 	console.log("i am loading");
 	chrome.storage.sync.get(["selectedColor"], (res) => {
 		document.body.style.backgroundColor = res.selectedColor;
+		document.querySelector("#color-code").innerHTML = "color code " + res.selectedColor;
 	});
 });
